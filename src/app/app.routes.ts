fea38e73e03edf16d45ff7ccb81cc87580626b4d
ypeain <yoonpyae26@gmail.com>
2025-02-19 15:41:23 +0630

    console state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { StateComponent } from './pages/state/state.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'state', component: StateComponent },
 ];
--- src/app/pages/state/state.component.ts
+++ src/app/pages/state/state.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StateService } from '../../core/services/state.service';
+import { RootModel } from '../../core/models/root.model';
+import { StateModel } from '../../core/models/state.model';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-state',
   imports: [],
   templateUrl: './state.component.html',
-  styleUrl: './state.component.scss'
+  styleUrl: './state.component.scss',
 })
-export class StateComponent {
+export class StateComponent implements OnInit {
+  states: StateModel[] = [];
 
+  constructor(private stateService: StateService) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.stateService.get().subscribe((res) => {
+      let result = res.data;
+      this.states = result.states as StateModel[];
+      console.log(result);
+    });
+  }
 }
