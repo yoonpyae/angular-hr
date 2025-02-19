7951087a3ddb0df3cdd33fc175c7cb4429b9c361
ypeain <yoonpyae26@gmail.com>
2025-02-19 16:36:44 +0630

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { StateComponent } from './pages/state/state.component';
+import { EntryComponent } from './pages/state/entry/entry.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'state', component: StateComponent },
+  {
+    path: 'state',
+
+    children: [
+      {
+        path: '',
+        component: StateComponent,
+      },
+      {
+        path: 'entry',
+        component: EntryComponent,
+      },
+    ],
+  },
 ];
