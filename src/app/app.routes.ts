4693709b00937853e6032f96264f74ef99261021
ypeain <yoonpyae26@gmail.com>
2025-02-19 15:04:22 +0630

    add files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
