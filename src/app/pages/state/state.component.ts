af36aabaf193184518e69e88b1c2950bd7e8b65b
ypeain <yoonpyae26@gmail.com>
2025-02-20 11:54:27 +0630

    add filter

--- src/app/pages/state/state.component.ts
+++ src/app/pages/state/state.component.ts
@@ -7,6 +7,11 @@ import { ButtonModule } from 'primeng/button';
 import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
+import { Table } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectModule } from 'primeng/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-state',
@@ -16,13 +21,19 @@ import { InputTextModule } from 'primeng/inputtext';
     IconFieldModule,
     InputIconModule,
     InputTextModule,
+    CommonModule,
+    SelectModule,
+    MultiSelectModule,
+    TagModule,
   ],
   templateUrl: './state.component.html',
   styleUrl: './state.component.scss',
 })
 export class StateComponent implements OnInit {
   states: StateModel[] = [];
+  loading: boolean = true;
 
+  activityValues: number[] = [0, 100];
   constructor(private stateService: StateService) {}
 
   ngOnInit(): void {
@@ -34,5 +45,9 @@ export class StateComponent implements OnInit {
       let result = res.data;
       this.states = result.states as StateModel[];
     });
+  }
+
+  clear(table: Table) {
+    table.clear();
   }
 }
