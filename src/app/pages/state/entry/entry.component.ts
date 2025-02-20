65e53d1d950d4157a6ad78a1333d56c7ab7cfeb7
ypeain <yoonpyae26@gmail.com>
2025-02-20 14:16:00 +0630

    update code in entry

--- src/app/pages/state/entry/entry.component.ts
+++ src/app/pages/state/entry/entry.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-entry',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, InputTextModule],
   templateUrl: './entry.component.html',
-  styleUrl: './entry.component.scss'
+  styleUrl: './entry.component.scss',
 })
-export class EntryComponent {
+export class EntryComponent implements OnInit {
+  constructor() {}
 
+  private formBuilder = inject(FormBuilder);
+  statFrom = this.formBuilder.group({
+    stateId: [''],
+    stateName: [''],
+    stateNameMm: [''],
+  });
+
+  ngOnInit(): void {}
 }
