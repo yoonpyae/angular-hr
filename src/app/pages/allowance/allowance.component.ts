60baebd5da15e36302bc566d619a3df6adf0a137
ypeain <yoonpyae26@gmail.com>
2025-02-24 14:56:33 +0630

    get allowance

--- src/app/pages/allowance/allowance.component.ts
+++ src/app/pages/allowance/allowance.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
+import { AllowanceModel } from '../../core/models/allowance.model';
+import { PolicyService } from '../../core/services/policy.service';
 
 @Component({
   selector: 'app-allowance',
-  imports: [],
+  imports: [
+    RouterModule,
+    TableModule,
+    ButtonModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    CommonModule,
+    SelectModule,
+    MultiSelectModule,
+    TableModule,
+  ],
   templateUrl: './allowance.component.html',
-  styleUrl: './allowance.component.scss'
+  styleUrl: './allowance.component.scss',
 })
-export class AllowanceComponent {
+export class AllowanceComponent implements OnInit {
+  selectedAllowance!: AllowanceModel;
+  allowances: AllowanceModel[] = [];
+  loading: boolean = true;
 
+  activityValues: number[] = [0, 100];
+  constructor(private policyService: PolicyService, private rout: Router) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.policyService.get().subscribe((res) => {
+      let result = res.data;
+      this.allowances = result.allowances as AllowanceModel[];
+    });
+  }
 }
