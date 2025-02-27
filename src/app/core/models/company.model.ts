66f1ee79b92c82692712dc36a880c5e4bfb5a060
ypeain <yoonpyae26@gmail.com>
2025-02-27 13:31:11 +0630

    add company model

---
+++ src/app/core/models/company.model.ts
@@ -0,0 +1,26 @@
+export interface CompanyModel {
+  companyId: string;
+  companyName: string;
+  joinDate: string;
+  licenseNo: string;
+  contactPerson: string;
+  primaryPhone: string;
+  otherPhone: string;
+  email: string;
+  houseNo: string;
+  streetId: number;
+  streetName: string;
+  townshipId: number;
+  townshipName: string;
+  stateId: number;
+  stateName: string;
+  photo: null;
+  status: boolean;
+  createdOn: string;
+  createdBy: string;
+  updatedOn: null | string;
+  updatedBy: null | string;
+  deletedOn: null;
+  deletedBy: null;
+  remark: null;
+}
