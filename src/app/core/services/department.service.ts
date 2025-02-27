528ff15a324d71f08bd34ff72eed99780266447f
ypeain <yoonpyae26@gmail.com>
2025-02-27 11:04:20 +0630

    add get, getbyid and create in the department service

--- src/app/core/services/department.service.ts
+++ src/app/core/services/department.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RootModel } from '../models/root.model';
+import { enviornment } from '../../../environments/environment';
+import { DepartmentModel } from '../models/department.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DepartmentService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  get(): Observable<RootModel> {
+    let url: string = `${enviornment.apiUrl}/api/Departments`;
+    return this.http.get<RootModel>(url);
+  }
+
+  getByID(id: number): Observable<RootModel> {
+    let url: string = `${enviornment.apiUrl}/api/Departments/${id}`;
+    return this.http.get<RootModel>(url);
+  }
+
+  create(model: DepartmentModel): Observable<RootModel> {
+    let url: string = `${enviornment.apiUrl}/api/Departments`;
+    return this.http.post<RootModel>(url, JSON.stringify(model), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
