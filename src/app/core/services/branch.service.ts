a60a100ad4e2acf30d4cf7988b53cfdac7c90fb8
ypeain <yoonpyae26@gmail.com>
2025-02-27 13:40:09 +0630

    add branch service data

--- src/app/core/services/branch.service.ts
+++ src/app/core/services/branch.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RootModel } from '../models/root.model';
+import { enviornment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BranchService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  get(): Observable<RootModel> {
+    let url: string = `${enviornment.apiUrl}/api/Branches`;
+    return this.http.get<RootModel>(url);
+  }
+
+  getByID(id: number): Observable<RootModel> {
+    let url: string = `${enviornment.apiUrl}/api/Branches/${id}`;
+    return this.http.get<RootModel>(url);
+  }
+
+  getByCompanyID(id: string): Observable<RootModel> {
+    let url: string = `${enviornment.apiUrl}/api/Branches/by-companyId`;
+    return this.http.get<RootModel>(url);
+  }
 }
