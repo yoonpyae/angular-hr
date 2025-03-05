3aa7d1976dd21419166a0505b571a3165069e772
ypeain <yoonpyae26@gmail.com>
2025-03-05 15:18:19 +0630

    add construtor

--- src/app/pages/job-opening/job-opening-entry/job-opening-entry.component.ts
+++ src/app/pages/job-opening/job-opening-entry/job-opening-entry.component.ts
@@ -1,6 +1,7 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { EditorModule } from 'primeng/editor';
 import { InputTextModule } from 'primeng/inputtext';
@@ -8,6 +9,17 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { SelectModule } from 'primeng/select';
 import { ToastModule } from 'primeng/toast';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { BranchModel } from '../../../core/models/branch.model';
+import { ViCompanyModel } from '../../../core/models/company.model';
+import { DepartmentModel } from '../../../core/models/department.model';
+import { PositionModel } from '../../../core/models/position.model';
+import { JobOpeningModel } from '../../../core/models/job-opening.model';
+import { JobOpeningService } from '../../../core/services/job-opening.service';
+import { PositionService } from '../../../core/services/position.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BranchService } from '../../../core/services/branch.service';
+import { CompanyService } from '../../../core/services/company.service';
+import { DepartmentService } from '../../../core/services/department.service';
 
 @Component({
   selector: 'app-job-opening-entry',
@@ -25,8 +37,41 @@ import { ToggleSwitchModule } from 'primeng/toggleswitch';
   ],
   templateUrl: './job-opening-entry.component.html',
   styleUrl: './job-opening-entry.component.scss',
+  providers: [DatePipe, MessageService],
 })
 export class JobOpeningEntryComponent implements OnInit {
+  companies: ViCompanyModel[] = [];
+  selectedCompany!: ViCompanyModel;
+
+  branches: BranchModel[] = [];
+  selectedBranch!: BranchModel;
+
+  depts: DepartmentModel[] = [];
+  selectedDept!: DepartmentModel;
+
+  positions: PositionModel[] = [];
+  selectedPosition!: PositionModel;
+
+  joID: number = 0;
+  model!: JobOpeningModel;
+  isEdit: boolean = false;
+  isSubmitting: boolean = false;
+  modalVisible: boolean = false;
+  checked: boolean = false;
+  loading: boolean = false;
+
+  constructor(
+    private jobOpeningService: JobOpeningService,
+    private companyService: CompanyService,
+    private branchService: BranchService,
+    private deptService: DepartmentService,
+    private positionService: PositionService,
+    private route: ActivatedRoute,
+    private datepipe: DatePipe,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
+
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
