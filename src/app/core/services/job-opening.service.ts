419c9aab12062e00b686dc142168c63d1aa59800
ypeain <yoonpyae26@gmail.com>
2025-03-05 15:13:41 +0630

    add create in service

--- src/app/core/services/job-opening.service.ts
+++ src/app/core/services/job-opening.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RootModel } from '../models/root.model';
 import { enviornment } from '../../../environments/environment';
+import { JobOpeningModel } from '../models/job-opening.model';
+import { Header } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +20,14 @@ export class JobOpeningService {
   getbyid(id: number): Observable<RootModel> {
     let url: string = `${enviornment.apiUrl}/api/JobOpening/${id}`;
     return this.http.get<RootModel>(url);
+  }
+
+  create(id: number, model: JobOpeningModel): Observable<RootModel> {
+    let url: string = `${enviornment.apiUrl}/api/JobOpening`;
+    return this.http.post<RootModel>(url, JSON.stringify(model), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
