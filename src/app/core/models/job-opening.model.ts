2f3ee85c5132a1d49c721f2da38bb30f1ea0868a
ypeain <yoonpyae26@gmail.com>
2025-03-05 15:22:08 +0630

    change up and down

--- src/app/core/models/job-opening.model.ts
+++ src/app/core/models/job-opening.model.ts
@@ -1,3 +1,24 @@
+export interface JobOpeningModel {
+  id: number;
+  title: string;
+  description: string;
+  noOfApplicants: number;
+  startOn: null;
+  endOn: null;
+  companyId: string;
+  branchId: number;
+  deptId: number;
+  positionId: number;
+  openingStatus: boolean;
+  createdOn: null | string;
+  createdBy: null | string;
+  updatedOn: null | string;
+  updatedBy: null | string;
+  deletedOn: null | string;
+  deletedBy: null | string;
+  remark: null | string;
+}
+
 export interface ViJobOpeningModel {
   id: number;
   title: string;
@@ -13,27 +34,6 @@ export interface ViJobOpeningModel {
   deptName: string;
   positionId: number;
   positionName: string;
-  openingStatus: boolean;
-  createdOn: null | string;
-  createdBy: null | string;
-  updatedOn: null | string;
-  updatedBy: null | string;
-  deletedOn: null | string;
-  deletedBy: null | string;
-  remark: null | string;
-}
-
-export interface JobOpeningModel {
-  id: number;
-  title: string;
-  description: string;
-  noOfApplicants: number;
-  startOn: null;
-  endOn: null;
-  companyId: string;
-  branchId: number;
-  deptId: number;
-  positionId: number;
   openingStatus: boolean;
   createdOn: null | string;
   createdBy: null | string;
