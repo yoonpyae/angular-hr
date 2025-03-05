6b4683ffff034f38a8d1451b5ef50904979609b9
ypeain <yoonpyae26@gmail.com>
2025-03-05 15:08:19 +0630

    create jobOpeningEntry files

---
+++ src/app/pages/job-opening/job-opening-entry/job-opening-entry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-opening-entry',
+  imports: [],
+  templateUrl: './job-opening-entry.component.html',
+  styleUrl: './job-opening-entry.component.scss'
+})
+export class JobOpeningEntryComponent {
+
+}
