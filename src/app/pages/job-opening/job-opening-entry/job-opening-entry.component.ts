2858610eb94b698e8e191615898b17da0300543e
ypeain <yoonpyae26@gmail.com>
2025-03-05 15:15:02 +0630

    import modules

--- src/app/pages/job-opening/job-opening-entry/job-opening-entry.component.ts
+++ src/app/pages/job-opening/job-opening-entry/job-opening-entry.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { EditorModule } from 'primeng/editor';
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { SelectModule } from 'primeng/select';
+import { ToastModule } from 'primeng/toast';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
   selector: 'app-job-opening-entry',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+    ToggleSwitchModule,
+    ProgressSpinnerModule,
+    EditorModule,
+    SelectModule,
+  ],
   templateUrl: './job-opening-entry.component.html',
-  styleUrl: './job-opening-entry.component.scss'
+  styleUrl: './job-opening-entry.component.scss',
 })
-export class JobOpeningEntryComponent {
-
+export class JobOpeningEntryComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
