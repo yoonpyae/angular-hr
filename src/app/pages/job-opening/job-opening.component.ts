fbec95e0188b7a9ac142e531e082384770f64134
ypeain <yoonpyae26@gmail.com>
2025-03-05 14:41:46 +0630

    add jobOpening

---
+++ src/app/pages/job-opening/job-opening.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-opening',
+  imports: [],
+  templateUrl: './job-opening.component.html',
+  styleUrl: './job-opening.component.scss'
+})
+export class JobOpeningComponent {
+
+}
