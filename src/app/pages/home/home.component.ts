d186a3612e93f0abff8287a3c2f1052a21ca4100
ypeain <yoonpyae26@gmail.com>
2025-03-10 09:33:37 +0630

    Tab and TabPanel testing

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, RouterModule, TabsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
