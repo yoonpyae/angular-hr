738242af5095065c680d73cfa5e8e7a419a89bb6
ypeain <yoonpyae26@gmail.com>
2025-03-10 09:45:45 +0630

    change content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { TabsModule } from 'primeng/tabs';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule, RouterModule, TabsModule, CommonModule],
+  imports: [ButtonModule, RouterModule, TabsModule, CommonModule, CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
